915fd1ecc7251a4c9edaf5819ab1f1c0901610f2
kctoh <kwangchern@gmail.com>
2017-06-24 20:53:08 +0800

    First commit

---
+++ plot1.R
@@ -0,0 +1,13 @@
+#Read data from text file
+data<-read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, na.strings="?")
+
+#Target data from 2007-02-01 to 2007-02-02
+
+dates<- data$Date=="1/2/2007"|data$Date=="2/2/2007"
+targetdata<-data[dates,]
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+#Global Active Power histogram plot and output as png
+hist(targetdata$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+dev.copy(png, file = "plot1.png")
+dev.off()
